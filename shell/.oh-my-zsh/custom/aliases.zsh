3de6ec84d1d8fc978f781f88e61f09508a71d39d
Arvind Vanthavasi <arvind.vanthavasi@gmail.com>
2020-12-22 15:15:01 +0000

    Added custom aliases for oh-my-zsh

---
+++ shell/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,12 @@
+# source reloads
+alias reload.zsh="source ~/.zshrc"
+alias reload.profile="source ~/.bash_profile"
+alias reload.term="source ~/.bash_profile && source ~/.zshrc"
+
+
+# ls
+alias ll="ls -AlhG"
+
+
+# tmux
+alias tmux="xterm-256color tmux"
