f46bffd1bf14bbf55b0c7eca887e8161175d0450
Arvind Vanthavasi <arvind.vanthavasi@gmail.com>
2023-12-14 20:03:20 -0500

    Added rust plugins

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,12 +4,21 @@ local on_attach = config.on_attach
 local capabilities = config.capabilities
 
 local lspconfig = require("lspconfig")
-local servers = { "pyright" }
 
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup = {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-end
-
+lspconfig.pyright.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		pyright = {
+			autoImportCompletion = true,
+		},
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "openFilesOnly",
+				useLibraryCodeForTypes = true,
+				typeCheckingMode = "off",
+			},
+		},
+	},
+})
--- .config/nvim/lua/custom/plugins/lsp/mason.lua
+++ .config/nvim/lua/custom/plugins/lsp/mason.lua
@@ -1,43 +1,45 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "pyright",
-        "lua_ls",
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true,
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"pyright",
+				"lua_ls",
+				"rust-analyzer",
+				"codelldb",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true,
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier",
-        "stylua", -- lua formatter
-        "isort", -- python import sorter
-        "black", -- python formatter
-        "mypy", -- python linter
-        "ruff", -- python linter and formatter 
-        "pylint", -- python linter
-      }
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua", -- lua formatter
+				"isort", -- python import sorter
+				"black", -- python formatter
+				"mypy", -- python linter
+				"ruff", -- python linter and formatter
+				"pylint", -- python linter
+			},
+		})
+	end,
 }
